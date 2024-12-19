8461126dd389acbc725790a4084f25f143bb674e
reverseCode23932 <gamerxdlike0@gmail.com>
2024-12-19 18:27:46 +0200

    Delete schoolqrcode directory

--- schoolqrcode/schoolqr/home/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import homepage, delete_item
-
-urlpatterns = [
-    path('', homepage),
-    path('delete/<int:id>/', delete_item),
-]
--- schoolqrcode/schoolqr/login/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import get_form, success, remove
-
-urlpatterns = [
-    path('', get_form),
-    path('<int:id>/', success),
-    path('delete_this/<int:id>/', remove)
-]
